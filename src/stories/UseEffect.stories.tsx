c355ca34c9f761c8c14dc26333593d678843e954
Kolibri2048 <blak_fog@mail.ru>
2024-07-02 14:58:22 +0200

    save

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useMemo, useReducer, useState} from "react";
+import {log} from "util";
 
 export default {
     title: 'useEffect demo'
@@ -8,38 +9,82 @@ export default {
 
 
 
-export const SimpleExample = () => {
+// export const SimpleExample = () => {
+//
+//     const [fake, setFake] = useState(1)
+//     const [counter, setCounter] = useState(1)
+//
+//     console.log('SimpleExample')
+//
+//     useEffect(() => {
+//         debugger
+//         console.log('UseEffect')
+//         document.title = counter.toString()
+//         // api.getUsr().then('')
+//         // setInterval
+//         // indexedDB
+//         // document.getElementID
+//         // document.title = 'User'
+//     }, [counter]) // дл€ того чтобы наш useEffect не срабатывал всЄ врем€ когда это не нужно. Ќадо передать зависимоть
+//                 // ≈сли мы передадим пустой массив то он сработает ровно один раз при монтирование компоненты
+//
+//     useEffect(() => {
+//         console.log('useEffect every render')
+//         document.title = counter.toString()
+//     })
+//
+//
+//     useEffect(() => {
+//         console.log('UseEffect only first render (componentDidMount)')
+//         document.title = counter.toString()
+//     })
+//
+//     useEffect(() => {
+//         console.log('UseEffect only first render and every counter change')
+//         document.title = counter.toString()
+//     }, [counter])
+//
+//     return<>
+//         Hello, {counter} {fake}
+//         <button onClick={() => setCounter(counter + 1)}>+</button>
+//         <button onClick={() => setFake(fake + 1)}>+</button>
+//
+//     </>
+// }
+
+export const SetTimeoutExp = () => {
 
     const [fake, setFake] = useState(1)
     const [counter, setCounter] = useState(1)
 
-    console.log('SimpleExample')
+    console.log('SetTimeoutExp')
+
+
+
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         console.log('setTimeout')
+    //         document.title = counter.toString()
+    //
+    //     }, 1000)
+    // }, [counter])
 
     useEffect(() => {
-        debugger
-        console.log('UseEffect')
-        document.title = counter.toString()
-        // api.getUsr().then('')
-        // setInterval
-        // indexedDB
-        // document.getElementID
-        // document.title = 'User'
-    }, [counter]) // дл€ того чтобы наш useEffect не срабатывал всЄ врем€ когда это не нужно. Ќадо передать зависимоть
-                // ≈сли мы передадим пустой массив то он сработает ровно один раз при монтирование компоненты
-    useEffect(() => {
-        console.log('UseEffect only first render (componentDidMount)')
-        document.title = counter.toString()
-    })
+        setInterval(() => {
+            console.log('tick ' + counter)
+            setCounter((state) => state + 1)
 
-    useEffect(() => {
-        console.log('UseEffect only first render and every counter change')
-        document.title = counter.toString()
-    }, [counter])
+        }, 1000)
+    }, [])
+
+
+
+
 
     return<>
-        Hello, {counter} {fake}
-        <button onClick={() => setCounter(counter + 1)}>+</button>
-        <button onClick={() => setFake(fake + 1)}>+</button>
+        Hello, counter: {counter} - fake: {fake}
+        {/*<button onClick={() => setCounter(counter + 1)}>+</button>*/}
+        {/*<button onClick={() => setFake(fake + 1)}>+</button>*/}
 
     </>
 }
